f9e68085649e41ab399c176740f80c6fc94227c3
Ben Ohayon <ben1679@gmail.com>
2022-12-31 12:15:36 +0200

    added socials for hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import footerLinkedinImage from '../../../assets/linkedin-icon.png'
+import footerGithubImage from '../../../assets/github-icon.png'
 
 import './Hero.scss'
+
+const socials = {
+  'https://www.linkedin.com/in/benohayon': footerLinkedinImage,
+  'https://github.com/BenOhayon': footerGithubImage
+}
 
 export default function Hero() {
   return (
@@ -9,7 +16,11 @@ export default function Hero() {
       <div className="hero-subtitle">A Fullstack Web Developer</div>
 
       <div className="hero-socials">
-
+        {Object.keys(socials).map(social => {
+          return <a key={social} href={social} target='_blank' className='hero-social-link'>
+            <img src={socials[social]} alt='social-item-image' />
+          </a>
+        })}
       </div>
     </section>
   )
