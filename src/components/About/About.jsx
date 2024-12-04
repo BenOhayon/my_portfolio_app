f5e5da326c771ea8179bad17cfd7d7987b0cf4af
Ben Ohayon <ben1679@gmail.com>
2024-12-04 11:01:25 +0200

    changed to software engineer

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,31 +6,36 @@ import { AOS_DURATION_MILLISECONDS } from '../../constants/general.constants'
 import { useAppContext } from '../MainApp/App'
 
 export default function About() {
-	const {
-		isMobile
-	} = useAppContext()
+    const {
+        isMobile
+    } = useAppContext()
 
-	return (
-		<section id='about' className='about-container'>
-			<div data-aos="fade-down" data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className='about-title header'>About Me</div>
-			<div className="about-content">
-				<div data-aos={isMobile ? "fade-up" : "fade-right"} data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className="about-summery about-subsection">
-					<div className="about-summery-title section-inner-title">Who am I?</div>
-					<p className='about-summery-desc paragraph-content'>
-						A <strong>web developer</strong> with 5 years of experience in software development.
-						I'm currently looking for my next challenge as a <strong>frontend or fullstack web developer</strong> in a place where I can learn, grow and contribute.
-						I have passion for coding and learning new technologies, independent and <strong>self learner</strong> with 'can do' attitude.
-					</p>
-					<p className='about-summery-desc paragraph-content'>
-						In addition, I developed a <strong>Node-RED</strong> app (a low code platform for IOT applications) for smart home management on a <strong>Raspberry PI</strong>.
-						This app connects to an <strong>ABB PLC</strong>, which is integrated to every electronic device in a house.
-					</p>
-				</div>
+    return (
+        <section id='about' className='about-container'>
+            <div data-aos="fade-down" data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className='about-title header'>About Me</div>
+            <div className="about-content">
+                <div data-aos={isMobile ? "fade-up" : "fade-right"} data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className="about-summery about-subsection">
+                    <div className="about-summery-title section-inner-title">Who am I?</div>
+                    <p className='about-summery-desc paragraph-content'>
+                        A <strong>software engineer</strong> with 5 years of experience.
+                        I'm currently looking for my next challenge as a <strong>software engineer (backend oriented)</strong> in a place where I can learn, grow and contribute.
+                        I have passion for coding and learning new technologies, independent and <strong>self learner</strong> with 'can do' attitude.
+                    </p>
+                    {/* <p className='about-summery-desc paragraph-content'>
+                        A <strong>web developer</strong> with 5 years of experience in software development.
+                        I'm currently looking for my next challenge as a <strong>frontend or fullstack web developer</strong> in a place where I can learn, grow and contribute.
+                        I have passion for coding and learning new technologies, independent and <strong>self learner</strong> with 'can do' attitude.
+                    </p> */}
+                    <p className='about-summery-desc paragraph-content'>
+                        In addition, I developed a <strong>Node-RED</strong> app (a low code platform for IOT applications) for smart home management on a <strong>Raspberry PI</strong>.
+                        This app connects to an <strong>ABB PLC</strong>, which is integrated to every electronic device in a house.
+                    </p>
+                </div>
 
-				<div data-aos={isMobile ? "fade-up" : "fade-left"} data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className="about-tech-stack about-subsection">
-					<TechStack />
-				</div>
-			</div>
-		</section>
-	)
+                <div data-aos={isMobile ? "fade-up" : "fade-left"} data-aos-duration={`${AOS_DURATION_MILLISECONDS}`} className="about-tech-stack about-subsection">
+                    <TechStack />
+                </div>
+            </div>
+        </section>
+    )
 }
--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -4,38 +4,36 @@ import awsImage from '../../../assets/aws.jpg'
 import reactImage from '../../../assets/react_image.png'
 import nodeImage from '../../../assets/node_image.png'
 import expressImage from '../../../assets/express_image.png'
-import jsImage from '../../../assets/js_image.png'
-import tsImage from '../../../assets/ts_image.png'
-import scssImage from '../../../assets/scss_image.png'
-import htmlImage from '../../../assets/html_image.png'
-import cssImage from '../../../assets/css_image.jpg'
 import rubyImage from '../../../assets/ruby_image.jpg'
 import railsImage from '../../../assets/rails_image.png'
 import javaImage from '../../../assets/java_image.jpg'
 import androidImage from '../../../assets/android_image.png'
 import nodeRedImage from '../../../assets/nodered_image.png'
-import githubImage from '../../../assets/github_image.png'
-import reduxImage from '../../../assets/redux_image.webp'
+import springBootImage from '../../../assets/spring-image.png'
+import dockerImage from '../../../assets/docker-image.webp'
+import pythonImage from '../../../assets/python-image.png'
+import flaskImage from '../../../assets/flask-image.png'
+import redisImage from '../../../assets/redis-image.webp'
+import mongoImage from '../../../assets/mongo-image.webp'
+import kafkaImage from '../../../assets/kafka-image.png'
 
 import './TechStack.scss'
 
 const techStack = {
-	'React': reactImage,
-	'Redux': reduxImage,
+	'Java': javaImage,
+    'Spring Boot': springBootImage,
 	'Node': nodeImage,
 	'Express': expressImage,
-	'JavaScript': jsImage,
-	'TypeScript': tsImage,
-	'SCSS': scssImage,
-	'CSS': cssImage,
-	'HTML': htmlImage,
+    'Python': pythonImage,
+    'Flask': flaskImage,
+    'Kafka': kafkaImage,
+    'MongoDB': mongoImage,
+    'Redis': redisImage,
+    'Docker': dockerImage,
 	'Ruby': rubyImage,
 	'Rails': railsImage,
-	'Java': javaImage,
 	'Android': androidImage,
 	'Node-RED': nodeRedImage,
-	'AWS': awsImage,
-	'GitHub': githubImage
 }
 
 export default function TechStack() {
