1421d16d1395f4c66270a4773dc9921178f9a098
Ben Ohayon <ben1679@gmail.com>
2025-06-06 00:13:22 +0300

    updated work experience

--- src/constants/general.constants.ts
+++ src/constants/general.constants.ts
@@ -2,7 +2,6 @@ import awsImage from '../../assets/aws.jpg'
 import reactImage from '../../assets/react_image.png'
 import nodeImage from '../../assets/node_image.png'
 import expressImage from '../../assets/express_image.png'
-import jsImage from '../../assets/js_image.png'
 import tsImage from '../../assets/ts_image.png'
 import scssImage from '../../assets/scss_image.png'
 import htmlImage from '../../assets/html_image.png'
@@ -13,7 +12,6 @@ import javaImage from '../../assets/java_image.jpg'
 import androidImage from '../../assets/android_image.png'
 import nodeRedImage from '../../assets/nodered_image.png'
 import githubImage from '../../assets/github_image.png'
-import reduxImage from '../../assets/redux_image.webp'
 import springBootImage from '../../assets/spring-image.png'
 import dockerImage from '../../assets/docker-image.webp'
 import pythonImage from '../../assets/python-image.png'
@@ -21,6 +19,8 @@ import flaskImage from '../../assets/flask-image.png'
 import redisImage from '../../assets/redis-image.webp'
 import mongoImage from '../../assets/mongo-image.webp'
 import kafkaImage from '../../assets/kafka-image.png'
+import mobxImage from '../../assets/mobx.svg'
+import styledComponentsImage from '../../assets/styled-components.png'
 
 import { ResumeData } from '../types'
 
@@ -28,200 +28,180 @@ export const AOS_DURATION_MILLISECONDS = 1000
 export const MOBILE_SCREEN_WIDTH_THRESHOLD_PX = 1085
 
 const sharedSkills = {
-    'Docker': dockerImage,
-    'AWS': awsImage,
-    'GitHub': githubImage,
-    'Node-RED': nodeRedImage,
+  Docker: dockerImage,
+  AWS: awsImage,
+  GitHub: githubImage,
+  'Node-RED': nodeRedImage,
 }
 
-export const fullstackDeveloperData: ResumeData = {
-    role: 'senior fullstack developer',
-    targetRoles: ['frontend developer', 'fullstack developer'],
-    techStack: {
-        'React': reactImage,
-        'Redux': reduxImage,
-        'Node': nodeImage,
-        'Express': expressImage,
-        'JavaScript': jsImage,
-        'TypeScript': tsImage,
-        'SCSS': scssImage,
-        'CSS': cssImage,
-        'HTML': htmlImage,
-        'Ruby': rubyImage,
-        'Rails': railsImage,
-        'Java': javaImage,
-        'Android': androidImage,
-        ...sharedSkills
+const frontendSkills = {
+  React: reactImage,
+  TypeScript: tsImage,
+  MobX: mobxImage,
+  'Styled Components': styledComponentsImage,
+  SCSS: scssImage,
+  CSS: cssImage,
+  HTML: htmlImage,
+  Android: androidImage,
+}
+
+const frontendWebSharedWorkExperience: ResumeData['workExperience'] = [
+  {
+    role: 'Frontend Web Developer',
+    company: "Cellxpert",
+    timePeriod: {
+      fromYear: 2025,
+      toYear: 'present'
     },
-    workExperience: [
-        {
-            role: 'frontend web developer',
-            company: "PaydIn",
-            timePeriod: {
-                fromYear: 2023,
-                toYear: 'present'
-            },
-            description: [
-                'Developing and designing a product for promoting sales in social medias',
-                'UI/UX development',
-                "Working with <b>React, Javascript, CSS3</b> and <b>Figma</b>"
-            ]
-        },
-        {
-            role: 'fullstack developer',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2021,
-                toYear: 2023
-            },
-            description: [
-                'Developed and designed web services for providing size recommendations',
-                'Developed <b>web apps & REST APIs</b>',
-                "Client side developed with <b>React, Javascript, SCSS</b> and <b>HTML</b>",
-                "Server side developed with <b>Ruby on Rails</b> and <b>PostgreSQL</b> database",
-                "Cloud services: <b>Heroku</b> and <b>AWS</b>",
-                "Working with <b>Zeplin</b>"
-            ]
-        },
-        {
-            role: 'Android Developer',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2019,
-                toYear: 2021
-            },
-            description: [
-                'Developed and maintained the size recommendation core mechanism',
-                'Developed and maintained MySize <b>mobile apps</b> and <b>mobile SDKs</b>',
-                "Working with <b>Java</b> and <b>Zeplin</b>"
-            ]
-        }
+    description: [
+      'Developing a product for affiliate management',
+      'Developed <b>responsive web apps</b>',
+      "Working with <b>React, TypeScript, MobX, Styled Components</b> and <b>Figma</b>"
     ]
+  },
+  {
+    role: 'frontend web developer',
+    company: "PaydIn",
+    timePeriod: {
+      fromYear: 2023,
+      toYear: 'present'
+    },
+    description: [
+      'Developing and designing a product for promoting sales in social medias',
+      'UI/UX development',
+      "Working with <b>React, Javascript, CSS3</b> and <b>Figma</b>"
+    ]
+  },
+]
+
+const androidSharedWorkExperience: ResumeData['workExperience'] = [
+  {
+    role: 'Android Developer',
+    company: "MySize",
+    timePeriod: {
+      fromYear: 2019,
+      toYear: 2021
+    },
+    description: [
+      'Developed and maintained the size recommendation core mechanism',
+      'Developed and maintained MySize <b>mobile apps</b> and <b>mobile SDKs</b>',
+      "Working with <b>Java</b> and <b>Zeplin</b>"
+    ]
+  },
+]
+
+export const fullstackDeveloperData: ResumeData = {
+  role: 'senior fullstack developer',
+  targetRoles: ['frontend developer', 'fullstack developer'],
+  techStack: {
+    ...frontendSkills,
+    Node: nodeImage,
+    Express: expressImage,
+    Ruby: rubyImage,
+    Rails: railsImage,
+    Java: javaImage,
+    ...sharedSkills
+  },
+  workExperience: [
+    ...frontendWebSharedWorkExperience,
+    {
+      role: 'fullstack developer',
+      company: "MySize",
+      timePeriod: {
+        fromYear: 2021,
+        toYear: 2023
+      },
+      description: [
+        'Developed and designed web services for providing size recommendations',
+        'Developed <b>web apps & REST APIs</b>',
+        "Client side developed with <b>React, Javascript, SCSS</b> and <b>HTML</b>",
+        "Server side developed with <b>Ruby on Rails</b> and <b>PostgreSQL</b> database",
+        "Cloud services: <b>Heroku</b> and <b>AWS</b>",
+        "Working with <b>Zeplin</b>"
+      ]
+    },
+    ...androidSharedWorkExperience,
+  ]
 }
 
 export const softwareDeveloperData: ResumeData = {
-    role: 'software developer',
-    targetRoles: ['software engineer backend oriented'],
-    techStack: {
-        'Java': javaImage,
-        'Spring Boot': springBootImage,
-        'Node': nodeImage,
-        'Express': expressImage,
-        'Python': pythonImage,
-        'Flask': flaskImage,
-        'Kafka': kafkaImage,
-        'MongoDB': mongoImage,
-        'Redis': redisImage,
-        'Ruby': rubyImage,
-        'Rails': railsImage,
-        ...sharedSkills
+  role: 'software developer',
+  targetRoles: ['software engineer backend oriented'],
+  techStack: {
+    Java: javaImage,
+    'Spring Boot': springBootImage,
+    Node: nodeImage,
+    Express: expressImage,
+    Python: pythonImage,
+    Flask: flaskImage,
+    Kafka: kafkaImage,
+    MongoDB: mongoImage,
+    Redis: redisImage,
+    Ruby: rubyImage,
+    Rails: railsImage,
+    ...sharedSkills
+  },
+  workExperience: [
+    {
+      role: 'Software Engineer (backend oriented)',
+      company: "PaydIn",
+      timePeriod: {
+        fromYear: 2023,
+        toYear: 'present'
+      },
+      description: [
+        'Developing and designing a product for promoting sales in social medias',
+        'Working with high-scale customers',
+        'Analyzing requirements & development',
+        'Part of 3 elite developers team',
+        "Working with <b>Figma</b>"
+      ]
     },
-    workExperience: [
-        {
-            role: 'Software Engineer (backend oriented)',
-            company: "PaydIn",
-            timePeriod: {
-                fromYear: 2023,
-                toYear: 'present'
-            },
-            description: [
-                'Developing and designing a product for promoting sales in social medias',
-                'Working with high-scale customers',
-                'Analyzing requirements & development',
-                'Part of 3 elite developers team',
-                "Working with <b>Figma</b>"
-            ]
-        },
-        {
-            role: 'Software Engineer (backend oriented)',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2021,
-                toYear: 2023
-            },
-            description: [
-                'Developed and designed web services for providing size recommendations',
-                'Implemented unique algorithms & patents',
-                "System design for scalability & security",
-                'Developed <b>web apps & REST APIs</b>',
-                "<b>PostgreSQL</b> database",
-                "Cloud services: <b>Heroku</b> and <b>AWS</b>",
-                "Working with <b>Zeplin</b>"
-            ]
-        },
-        {
-            role: 'Android Developer',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2019,
-                toYear: 2021
-            },
-            description: [
-                'Developed and maintained the size recommendation core mechanism',
-                'Developed and maintained MySize <b>mobile apps</b> and <b>mobile SDKs</b>',
-                "Working with <b>Java</b> and <b>Zeplin</b>"
-            ]
-        }
-    ]
+    {
+      role: 'Software Engineer (backend oriented)',
+      company: "MySize",
+      timePeriod: {
+        fromYear: 2021,
+        toYear: 2023
+      },
+      description: [
+        'Developed and designed web services for providing size recommendations',
+        'Implemented unique algorithms & patents',
+        "System design for scalability & security",
+        'Developed <b>web apps & REST APIs</b>',
+        "<b>PostgreSQL</b> database",
+        "Cloud services: <b>Heroku</b> and <b>AWS</b>",
+        "Working with <b>Zeplin</b>"
+      ]
+    },
+    ...androidSharedWorkExperience,
+  ]
 }
 
 export const frontendDeveloperData: ResumeData = {
-    role: 'frontend developer',
-    targetRoles: ['frontend developer'],
-    techStack: {
-        'React': reactImage,
-        'Redux': reduxImage,
-        'JavaScript': jsImage,
-        'TypeScript': tsImage,
-        'SCSS': scssImage,
-        'CSS': cssImage,
-        'HTML': htmlImage,
-        'Java': javaImage,
-        'Android': androidImage,
-        ...sharedSkills
+  role: 'frontend developer',
+  targetRoles: ['frontend developer'],
+  techStack: {
+    ...frontendSkills,
+    ...sharedSkills
+  },
+  workExperience: [
+    ...frontendWebSharedWorkExperience,
+    {
+      role: 'Frontend Web Developer',
+      company: "MySize",
+      timePeriod: {
+        fromYear: 2021,
+        toYear: 2023
+      },
+      description: [
+        'Developed and designed web services for providing size recommendations',
+        'Developed <b>responsive web apps</b>',
+        "Developed with <b>React, Javascript, SCSS</b> and <b>HTML</b>",
+        "Cloud services: <b>Heroku</b> and <b>AWS</b>",
+        "Working with <b>Zeplin</b>"
+      ]
     },
-    workExperience: [
-        {
-            role: 'Frontend Web Developer',
-            company: "PaydIn",
-            timePeriod: {
-                fromYear: 2023,
-                toYear: 'present'
-            },
-            description: [
-                'Developing and designing a product for promoting sales in social medias',
-                'UI/UX development',
-                'Developed <b>responsive web apps</b>',
-                "Working with <b>React, Javascript, CSS3, Material UI</b> and <b>Figma</b>"
-            ]
-        },
-        {
-            role: 'Frontend Web Developer',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2021,
-                toYear: 2023
-            },
-            description: [
-                'Developed and designed web services for providing size recommendations',
-                'Developed <b>responsive web apps</b>',
-                "Developed with <b>React, Javascript, SCSS</b> and <b>HTML</b>",
-                "Cloud services: <b>Heroku</b> and <b>AWS</b>",
-                "Working with <b>Zeplin</b>"
-            ]
-        },
-        {
-            role: 'Android Developer',
-            company: "MySize",
-            timePeriod: {
-                fromYear: 2019,
-                toYear: 2021
-            },
-            description: [
-                'Developed and maintained the size recommendation core mechanism',
-                'Developed and maintained MySize <b>mobile apps</b> and <b>mobile SDKs</b>',
-                "Working with <b>Java</b> and <b>Zeplin</b>"
-            ]
-        }
-    ]
+    ...androidSharedWorkExperience,
+  ]
 }
